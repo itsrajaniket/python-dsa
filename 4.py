1088696b254f22461f1bbc1b6f91b4a2fb9dbb39
itsrajaniket <itsrajaniket@gmail.com>
2026-07-16 19:33:05 +0530

    fx in # Is a Number Odd or Even?

--- 4.py
+++ 4.py
@@ -1,2 +1,9 @@
 # Is a Number Odd or Even? 
 
+x = int(input("Enter a number: "))
+
+if x % 2 == 0 :
+    print(f"The input number {x} is even")
+else :
+    print(f"The input number {x} is odd")
+    
