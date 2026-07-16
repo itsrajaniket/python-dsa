5a6621bcbbd4f858a2a0aad6fc5640cfa8604a9f
itsrajaniket <itsrajaniket@gmail.com>
2026-07-16 19:44:12 +0530

    # Find the Largest Among Three Numbers

---
+++ 5.py
@@ -0,0 +1,13 @@
+# Find the Largest Among Three Numbers 
+x = 1000
+y = 200
+z = 300
+
+
+if x > y and x > z :
+    print(x)
+elif y > x and y > z :
+    print(y)
+elif z > x and z > y :
+    print (z)
+    
