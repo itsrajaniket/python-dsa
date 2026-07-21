08d07588fe5745e829a2bf877bb176058d34acd0
itsrajaniket <itsrajaniket@gmail.com>
2026-07-22 00:42:31 +0530

    q6

---
+++ 6.py
@@ -0,0 +1,18 @@
+# Python Program to Check Prime Number 
+
+num = 5
+
+for i in range ( 2, num ):
+    if num % i == 0:
+        print ("its not prime")
+    elif num == 2:
+        print 
+
+
+
+# if num == 2 :
+#     print ("its prime")
+# elif num % 2 == 1 :
+#     print ("its prime")
+# else :
+#     print("not prime")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+if num == 2 :
+#     print ("its prime")
+# elif num % 2 == 1 :
+#     print ("its prime")
+# else :
+#     print("not prime")
