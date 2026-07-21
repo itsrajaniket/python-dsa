3395e1abbfa01137cb38a9d52467b6ce12ed9b72
itsrajaniket <itsrajaniket@gmail.com>
2026-07-22 01:49:47 +0530

    Exercise 3. String Indexing and Even Slicing

---
+++ 9.py
@@ -0,0 +1,8 @@
+# Exercise 3. String Indexing and Even Slicing
+
+word = "aniket"
+res = word[0: :2]
+print(res)
+for i in res:
+    print(i)
+
--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-if num == 2 :
-#     print ("its prime")
-# elif num % 2 == 1 :
-#     print ("its prime")
-# else :
-#     print("not prime")
